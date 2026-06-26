4a6ef3c37e7229bf0af1c6bd88881b3c20006d1f
suryaprakash <b.7993974026@gmail.com>
2026-06-26 11:39:59 +0530

    Add files via upload

---
+++ flask_project/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form.get('name')
+    city = request.form.get('city')
+    phone = request.form.get('phone')
+    return render_template('confirmation.html', name=name, city=city, phone=phone)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
